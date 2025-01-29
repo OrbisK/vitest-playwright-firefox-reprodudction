5938aaa820a7561165a1f2bfdbd0fbb3d75f6a95
OrbisK <robin.kehl@singular-it.de>
2025-01-29 13:53:22 +0100

    add reproduction code

--- vitest-example/HelloWorld.test.ts
+++ vitest-example/HelloWorld.test.ts
@@ -1,10 +1,39 @@
-import { expect, test } from 'vitest'
-import { render } from 'vitest-browser-vue'
-import HelloWorld from './HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import { useAnimate } from '@vueuse/core'
+import { describe, expect, it, vi } from 'vitest'
+import { shallowRef } from 'vue'
 
-test('renders name', async () => {
-  const { getByText } = render(HelloWorld, {
-    props: { name: 'Vitest' },
+describe('useAnimate', () => {
+  it('browser should support useAnimate', () => {
+    const wrapper = mount({
+      template: '<p ref="el">test</p>',
+      setup() {
+        const el = shallowRef<HTMLElement>()
+        const animate = useAnimate(el, { transform: 'rotate(360deg)' }, 100)
+
+        return { ...animate, el }
+      },
+    })
+    const vm = wrapper.vm
+
+    expect(vm.isSupported).toBe(true)
+    wrapper.unmount()
   })
-  await expect.element(getByText('Hello Vitest!')).toBeInTheDocument()
+
+  it('should be running', async () => {
+    const wrapper = mount({
+      template: '<p ref="el">test</p>',
+      setup() {
+        const el = shallowRef<HTMLElement>()
+        const animate = useAnimate(el, { transform: 'rotate(360deg)' }, 100)
+
+        return { ...animate, el }
+      },
+    })
+    const vm = wrapper.vm
+    await vi.waitFor(() => {
+      expect(vm.playState).toBe('running')
+    })
+    wrapper.unmount()
+  })
 })
